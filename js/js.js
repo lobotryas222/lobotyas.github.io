fccf4a72d30cd4334235aeb5d460c4a1c75d587f
Roma <60256776+lobotryas222@users.noreply.github.com>
2020-04-12 18:15:05 +0300

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,15 @@
+$ (function() {
+
+$ (".menuMEDIA").on ("click", function() {
+
+$ (".menu").slideToggle(300, function(){
+	
+	if ($ (this).css("display") === "none")
+		{$ (this).removeAttr("style");
+
+				}
+		});
+
+	} )
+
+});
